0afbd64b4b6c034063646f9e3935976a60ae38a6
Chris Urban <curban6@gmail.com>
2026-08-13 07:19:30 -0700

    Fix P residual parser handshake

--- rtl/mpeg2_new/mpeg2_h262_p_residual_pipeline.sv
+++ rtl/mpeg2_new/mpeg2_h262_p_residual_pipeline.sv
@@ -25,63 +25,34 @@ module mpeg2_h262_p_residual_probe
     output wire signed [15:0] residual_sample_value,
     output wire       probe_error
 );
-
 wire transform_block_done;
 wire [1:0] qfs_block_index;
-wire qfs_block_start;
-wire qfs_write_en;
+wire qfs_block_start,qfs_write_en,qfs_block_end;
 wire [5:0] qfs_write_index;
 wire signed [12:0] qfs_write_value;
-wire qfs_block_end;
 wire [4:0] quantiser_scale_code;
-wire q_scale_type;
-wire alternate_scan;
-wire parser_error;
-wire transform_error;
+wire q_scale_type,alternate_scan,parser_error,transform_error;
 wire [1:0] residual_sample_block_index_unused;
 
 mpeg2_h262_p_residual_parser parser
 (
-    .clk(clk), .reset(reset),
-    .stream_data(stream_data), .stream_valid(stream_valid),
-    .p_picture_expected(p_picture_expected),
-    .block_done(transform_block_done),
-    .decision_complete(decision_complete),
-    .residual_required(residual_required),
-    .residual_success(residual_success),
-    .qfs_block_index(qfs_block_index),
-    .qfs_block_start(qfs_block_start),
-    .qfs_write_en(qfs_write_en),
-    .qfs_write_index(qfs_write_index),
-    .qfs_write_value(qfs_write_value),
-    .qfs_block_end(qfs_block_end),
-    .quantiser_scale_code(quantiser_scale_code),
-    .q_scale_type(q_scale_type),
-    .alternate_scan(alternate_scan),
-    .probe_error(parser_error)
+ .clk(clk),.reset(reset),.stream_data(stream_data),.stream_valid(stream_valid),
+ .p_picture_expected(p_picture_expected),.transform_block_done(transform_block_done),
+ .decision_complete(decision_complete),.residual_required(residual_required),.residual_success(residual_success),
+ .qfs_block_index(qfs_block_index),.qfs_block_start(qfs_block_start),.qfs_write_en(qfs_write_en),
+ .qfs_write_index(qfs_write_index),.qfs_write_value(qfs_write_value),.qfs_block_end(qfs_block_end),
+ .quantiser_scale_code(quantiser_scale_code),.q_scale_type(q_scale_type),.alternate_scan(alternate_scan),
+ .probe_error(parser_error)
 );
-
 mpeg2_h262_p_non_intra_transform transform
 (
-    .clk(clk), .reset(reset),
-    .qfs_block_index(qfs_block_index),
-    .qfs_block_start(qfs_block_start),
-    .qfs_write_en(qfs_write_en),
-    .qfs_write_index(qfs_write_index),
-    .qfs_write_value(qfs_write_value),
-    .qfs_block_end(qfs_block_end),
-    .quantiser_scale_code(quantiser_scale_code),
-    .q_scale_type(q_scale_type),
-    .alternate_scan(alternate_scan),
-    .block_done(transform_block_done),
-    .first_sample_valid(first_sample_valid),
-    .first_sample_value(first_sample_value),
-    .residual_sample_valid(residual_sample_valid),
-    .residual_sample_block_index(residual_sample_block_index_unused),
-    .residual_sample_index(residual_sample_index),
-    .residual_sample_value(residual_sample_value),
-    .probe_error(transform_error)
+ .clk(clk),.reset(reset),.qfs_block_index(qfs_block_index),.qfs_block_start(qfs_block_start),
+ .qfs_write_en(qfs_write_en),.qfs_write_index(qfs_write_index),.qfs_write_value(qfs_write_value),
+ .qfs_block_end(qfs_block_end),.quantiser_scale_code(quantiser_scale_code),.q_scale_type(q_scale_type),
+ .alternate_scan(alternate_scan),.block_done(transform_block_done),.first_sample_valid(first_sample_valid),
+ .first_sample_value(first_sample_value),.residual_sample_valid(residual_sample_valid),
+ .residual_sample_block_index(residual_sample_block_index_unused),.residual_sample_index(residual_sample_index),
+ .residual_sample_value(residual_sample_value),.probe_error(transform_error)
 );
-
-assign probe_error = parser_error | transform_error;
+assign probe_error=parser_error|transform_error;
 endmodule
